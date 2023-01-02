7c33da8ab3d8f41d1e8fc17d2dc9e1fd8c0feb10
Harsh Kumar Jangir <111hkj111@gmail.com>
2023-01-02 14:42:11 +0530

    my first commit

---
+++ Assignments/Python_Programming_Assignment/.virtual_documents/Assignment 17.ipynb.py
@@ -0,0 +1,18 @@
+def sum_even(a,b,c):
+    sum=0
+    for i in range(a,b+1):
+        if iget_ipython().run_line_magic("c==0:", "")
+            sum=sum+i
+    if sum==0:
+        return f"No Number between {a} and {b} can be evenly divided by {c}"
+    else:
+            return sum
+sum_even(1,10,20)
+            
+
+
+if 12>13<14:
+    print(True)
+
+
+
